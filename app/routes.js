57d29b8d0626cfaac69bb378e5f4f8502c19acb6
Ben Watson <hello@benwatson.eu>
2019-10-24 15:01:59 +0100

    Dummy jobs added

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+const jobs = [
+  {
+    'id': 0,
+    'title': '',
+    'company': 'Tanesco',
+    'role': 'Junior Electrician',
+    'location': 'Dar es Salaam',
+    'description': 'A junior electrician with Level 3 qualifications required for role in Dar es Salaam'
+  },
+  {
+    'id': 1,
+    'title': '',
+    'company': 'Company 2',
+    'role': 'Apprentice Electrician',
+    'location': 'Mtwara',
+    'description': 'A motivated young electrician is needed for this family run company in Mtwara'
+  }
+]
+
 // Add your routes here - above the module.exports line
-// router.get('/v1/jobs', (req, res) => {
-//   res.render('v1/jobs', {
-//   })
-// })
+router.get('/v1/jobs', (req, res) => {
+  res.render('v1/jobs', {
+    jobs: jobs
+  })
+})
 
 router.get(['/v1/job', '/v1/job/1'], (req, res) => {
   res.render('v1/job', {
