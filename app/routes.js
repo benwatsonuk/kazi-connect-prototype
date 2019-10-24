8d90ee8dad1deb7a252173dd5c99fe8dcaa13c83
Ben Watson <hello@benwatson.eu>
2019-10-24 14:56:17 +0100

    Initial basic example of Kazi Connect

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// router.get('/v1/jobs', (req, res) => {
+//   res.render('v1/jobs', {
+//   })
+// })
+
+router.get(['/v1/job', '/v1/job/1'], (req, res) => {
+  res.render('v1/job', {
+    job: 1
+  })
+})
+
+router.get('/v1/confirmation', (req, res) => {
+  let employerName = req.query.employerName || 'TANESCO'
+  res.render('v1/confirmation', {
+    employerName: employerName
+  })
+})
 
 module.exports = router
