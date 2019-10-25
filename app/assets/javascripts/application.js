87d9a689519da58083000fd27c4643e34cf4b7ef
Ben Watson <hello@benwatson.eu>
2019-10-25 08:44:26 +0100

    Back button and some JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,17 @@ $('.job-box').click(function (e) {
   window.location = $(this).find('a').attr('href')
 })
 
+$('#code').focus()
+$('#login .govuk-button').hide()
+
+$('#login #code').keypress(function (e) {
+  console.log($(this).val())
+  let value = $(this).val()
+  if (value.length >= 3) {
+    $('.govuk-button').fadeIn('slow')
+  }
+})
+
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
